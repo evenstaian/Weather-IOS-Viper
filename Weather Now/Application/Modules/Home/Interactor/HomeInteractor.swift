9cd562db37d2f90d261865715c4441a98816cb91
Evens Taian <evenstaian@gmail.com>
2020-04-27 02:23:37 -0400

    init home interactor and viper interactor model

---
+++ Weather Now/Application/Modules/Home/Interactor/HomeInteractor.swift
@@ -0,0 +1,26 @@
+//
+//  HomeInteractor.swift
+//  Weather Now
+//
+//  Created by Evens Taian on 27/04/20.
+//  Copyright © 2020 Evens Taian. All rights reserved.
+//
+
+import Foundation
+
+class HomeInteractor: HomeInteractorType {
+    
+    //let homeDataManager
+    weak var delegate: HomeInteractorDelegate?
+    
+    init(){
+        
+    }
+    
+    // MARK: API
+    func loadWheather() {
+        
+    }
+    
+    
+}
---
+++ Weather Now/Application/Modules/Home/Interactor/HomeInteractorType.swift
@@ -0,0 +1,22 @@
+//
+//  HomeInteractorType.swift
+//  Weather Now
+//
+//  Created by Evens Taian on 27/04/20.
+//  Copyright © 2020 Evens Taian. All rights reserved.
+//
+
+import Foundation
+
+enum HomeInteractorError: Error {
+    case loadError
+    case networkError
+}
+
+protocol HomeInteractorDelegate: class {
+    func didUpdate()
+}
+
+protocol HomeInteractorType {
+    func loadWheather()
+}
