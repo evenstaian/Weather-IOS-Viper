bfe36f85bb093d15384156164dc9d72aa98a4a14
Evens Taian <evenstaian@gmail.com>
2020-04-27 02:48:06 -0400

    init environment

---
+++ Weather Now/Application/Environment/Environment.swift
@@ -0,0 +1,19 @@
+//
+//  Environment.swift
+//  Weather Now
+//
+//  Created by Evens Taian on 27/04/20.
+//  Copyright © 2020 Evens Taian. All rights reserved.
+//
+
+import Foundation
+
+class Environment: EnvironmentType {
+    
+    let loadURLString = "https://api.openweathermap.org/data/2.5/weather"
+    
+    // TODO Function
+    var homeDataManager: String = "TODO"
+    
+    
+}
---
+++ Weather Now/Application/Environment/EnvironmentConsumer.swift
@@ -0,0 +1,18 @@
+//
+//  EnvironmentConsumer.swift
+//  Weather Now
+//
+//  Created by Evens Taian on 27/04/20.
+//  Copyright © 2020 Evens Taian. All rights reserved.
+//
+
+import Foundation
+
+class EnvironmentConsumer {
+    
+    let environment: EnvironmentType
+    
+    init(environment: EnvironmentType){
+        self.environment = environment
+    }
+}
